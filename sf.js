57868d6c3dbb73eb9a94b8356ce748c0c474361e
wcclose <96881661+wcclose@users.noreply.github.com>
2024-07-03 00:39:17 +0800

    Add files via upload

--- sf.js
+++ sf.js
@@ -1,1 +1,9 @@
+const url = 'https://mcs-mimp-web.sf-express.com/mcs-mimp/commonPost/~memberNonactivity~integralTaskSignPlusService~automaticSignFetchPackage';
 
+if ($request && $request.url === url) {
+  const cookies = $request.headers['Cookie'];
+  console.log(`抓取到的Cookie: ${cookies}`);
+  $notification.post("Cookie抓取", "已抓取到Cookie", cookies);
+}
+
+$done({});
