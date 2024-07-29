bfd1a03ab8b795175aeaf49b0ee636bf55c49e98
wcclose <96881661+wcclose@users.noreply.github.com>
2024-07-29 17:06:20 +0800

    Delete sf.js

--- sf.js
+++
@@ -1,9 +0,0 @@
-const url = 'https://mcs-mimp-web.sf-express.com/mcs-mimp/commonPost/~memberNonactivity~integralTaskSignPlusService~automaticSignFetchPackage';
-
-if ($request && $request.url === url) {
-  const cookies = $request.headers['Cookie'];
-  console.log(`抓取到的Cookie: ${cookies}`);
-  $notification.post("Cookie抓取", "已抓取到Cookie", cookies);
-}
-
-$done({});
